d84f1c3855e255dfded63b9494342398377250e6
BerlynSG <noberlynsanchez@hotmail.com>
2024-07-19 02:19:38 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'poliseg_20220660',
+  webDir: 'www'
+};
+
+export default config;
